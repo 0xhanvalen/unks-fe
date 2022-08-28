d8c0d5951a677fa593ae8bd37621bb833b417c04
Han Valen <han@0xhanvalen.com>
2022-08-28 07:37:11 -0700

    fixed times back

--- pages/index.js
+++ pages/index.js
@@ -175,7 +175,7 @@ export default function Home() {
                   Your currently connected wallet is: <br /> {address}
                 </p>
                 {
-                  Date.now() >= 1661738400000 && <>
+                  Date.now() >= 1661695200000 && <>
                     <input
                       type="number"
                       value={amountToMint}
@@ -185,7 +185,7 @@ export default function Home() {
                   </>
                 }
                 {
-                  Date.now() >= 1661695200000 && Date.now() < 1661738400000 && isAllowlist && (
+                  Date.now() >= 1661738400000 && Date.now() < 1661695200000 && isAllowlist && (
                     <>
                       <input
                         type="number"
