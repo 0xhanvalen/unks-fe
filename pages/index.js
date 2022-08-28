18bce0bf77bbd6ce7f3e1a051eef28c38e63db31
Han Valen <han@0xhanvalen.com>
2022-08-28 07:44:56 -0700

    pls work

--- pages/index.js
+++ pages/index.js
@@ -175,7 +175,7 @@ export default function Home() {
                   Your currently connected wallet is: <br /> {address}
                 </p>
                 {
-                  Date.now() >= 1661695200000 && <>
+                  Date.now() >= 1661738400000 && <>
                     <input
                       type="number"
                       value={amountToMint}
